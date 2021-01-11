ccb5390e06a4869a84f328644298f595735be12d
dogclassifier <75272150+dogclassifier@users.noreply.github.com>
2021-01-11 23:49:02 +0530

    Update indexScript.js

--- indexScript.js
+++ indexScript.js
@@ -523,6 +523,6 @@ function vidGotResults(error, results2) {
   
   document.getElementById("Class2").innerHTML="Breed: "+cresult;
   document.getElementById("Class2").style.color="#00ff00";
- 
+ doglbl="";
 
 }
