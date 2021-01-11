bbb3d8b8d3c0740b7691387b44d1a6cb57ff90e3
dogclassifier <75272150+dogclassifier@users.noreply.github.com>
2021-01-11 18:14:22 +0530

    Update indexScript.js

--- indexScript.js
+++ indexScript.js
@@ -521,6 +521,7 @@ function vidGotResults(error, results2) {
   
   document.getElementById("Class2").innerHTML="Breed: "+cresult;
   document.getElementById("Class2").style.color="#00ff00";
-doglbl="";	
+doglbl="";
+	 if (menuMode==3 & play==0 & doglbl!="dog") {playClassify();}
 
 }
